9e032890e1ab1085ae5999419d2ae4d9bca38255
Jonah King <jonahkingcs@gmail.com>
2026-01-29 15:43:46 +0000

    change navbar style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -70,7 +70,7 @@ export default function NavBar() {
 
   return (
     // NAV WRAPPER (fixed bar at the top)
-    <nav className="bg-brand-cream dark:bg-brand-brown fixed w-full z-50 top-0 border-b border-default border-brand-blue dark:border-brand-blue">
+    <nav className="bg-brand-cream dark:bg-brand-brown shadow-md fixed w-full z-50 top-0">
       <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
 
         {/* LEFT SIDE: Logo / Brand */}
