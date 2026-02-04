b0b8d17ebbdc59e7620446879eb5694d5a8e3e25
NLJorber <nljordanbaker@gmail.com>
2026-02-04 11:14:00 +0000

    modetoggle close burger menu on click update

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -12,24 +12,33 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+export function ModeToggle({ onModeSelected }) {
   const { setTheme } = useTheme()
 
+
 const itemClass =
-  "data-[highlighted]:!bg-red-500 data-[highlighted]:!text-brand-brown " +
+  "data-[highlighted]:!bg-brand-cream data-[highlighted]:!text-brand-brown " +
   "focus:!bg-brand-cream focus:!text-brand-brown " +
   "dark:data-[highlighted]:!bg-brand-brown dark:data-[highlighted]:!text-brand-cream " +
   "dark:focus:!bg-brand-brown dark:focus:!text-brand-cream"
 
+const selectTheme = (theme) => {
+    setTheme(theme)
+    onModeSelected?.()
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon"
         className="bg-brand-orange dark:bg-brand-blue text-brand-cream">
+
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+
           <span className="sr-only">Toggle theme</span>
+
         </Button>
       </DropdownMenuTrigger>
 
@@ -38,16 +47,24 @@ const itemClass =
   className="z-[9999] bg-brand-blue dark:bg-brand-orange border border-brand-brown shadow-lg dark:border-brand-brown text-brand-cream dark:text-brand-cream
   w-20 min-w-0 p-1"
 >
-  <DropdownMenuItem onClick={() => setTheme("light")}
+  <DropdownMenuItem 
+  className={itemClass}
+  onClick={() => selectTheme("light")}
     
     >Light</DropdownMenuItem>
-  <DropdownMenuItem onClick={() => setTheme("dark")}
+
+  <DropdownMenuItem 
+  className={itemClass}
+  onClick={() => selectTheme("dark")}
     
     >Dark</DropdownMenuItem>
-  <DropdownMenuItem onClick={() => setTheme("system")}
+  <DropdownMenuItem 
+  className={itemClass}
+  onClick={() => selectTheme("system")}
     
-    >System</DropdownMenuItem>
-</DropdownMenuContent>
+    >System
+    </DropdownMenuItem>
+    </DropdownMenuContent>
     </DropdownMenu>
   )
 }
